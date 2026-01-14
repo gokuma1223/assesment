618f638da2a24a2196500d541813125c34b5eb88
gokuma1223 <gokuma1223@gmail.com>
2026-01-14 15:44:29 +0900

    Bootstrap適用

--- assessment.js
+++ assessment.js
@@ -20,14 +20,25 @@ assessmentButton.addEventListener("click", () => {
   }
 
   // 診断結果表示エリアの作成
-  resultDivision.innerText = "";
-  const heading = document.createElement("h3");
-  heading.innerText = "診断結果";
-  resultDivision.appendChild(heading);
+
+  // headerDivisionの作成
+  const headerDivision = document.createElement("div");
+  headerDivision.setAttribute("class", "card-header text-bg-primary");
+  headerDivision.innerText = "診断結果";
+
+  // bodyDivisionの作成
+  const bodyDivision = document.createElement("div");
+  bodyDivision.setAttribute("class", "card-body");
 
   const paragraph = document.createElement("p");
+  paragraph.setAttribute("class", "card-text");
   paragraph.innerText = assessment(userName);
-  resultDivision.appendChild(paragraph);
+  bodyDivision.appendChild(paragraph);
+
+  resultDivision.setAttribute("class", "card");
+
+  resultDivision.appendChild(headerDivision);
+  resultDivision.appendChild(bodyDivision);
 
   // ツイートエリアの作成
   tweetDivision.innerText = "";
